1c49a6df79c3cd7a8b604d1ff16f2c07e9e88793
Vladimir Druzhinin <lvladimirdruzhininl@gmail.com>
2022-01-16 14:38:21 +0300

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -1,119 +1,29 @@
-#include <iostream>
-#include <type_traits>
-#include "concepts.h"
-
 #include "state.h"
+using namespace Concept;
 #include <vector>
-
-#include <unordered_map>
-
-#include <fmt/core.h>
-
-
-
-//template<typename T>
-//concept HasMemberPushBack = requires(T type) {
-//    type.push_back(auto);
-//};
-
-template<typename T>
-concept Int = std::is_integral_v<T>;
-
-template<typename T>
-concept MyConcept = true;
-
-void test(Int auto type) {
-    std::cout << "Integral type: " << type << std::endl;
-}
-
-void test(auto type) {
-    std::cout << "none Integral type: " << type << std::endl;
-}
-
-template<Int T>
-void test2(T type) {
-    std::cout << "Integral type: " << type << std::endl;
-}
-
-template<typename T>
-void test2(T type) {
-    std::cout << "none Integral type: " << type << std::endl;
-}
-
-void test3(auto type) requires std::is_integral_v<decltype(type)> {
-    std::cout << "Integral type: " << type << std::endl;
-}
-
-void test3(auto type) {
-    std::cout << "none Integral type" << type << std::endl;
-}
-
-MyConcept auto initMyConcept() {
-    return 0;
-}
-
-template<typename T>
-requires MyConcept<T>
-void func(T type);
-
-
-//template<typename T>
-//concept Number = std::integral<T> or std::floating_point<T>;
-//
-//void dump(Number auto val, std::ostream& out) {
-//    out << fmt::format("Number = {}\n", val);
-//}
-
-
-
-//template<typename T>
-//concept String = requires (T str) {
-//    std::string_view{str};
-//};
-
-//void dump(auto val, std::ostream& out) {
-//    out << fmt::format("String = {}\n", val);
-//}
-
-//void dump(int val, std::ostream& out) {
-//    out << fmt::format("Number = {}\n", val);
-//}
 
 
 int main()
 {
-//    std::unique_ptr<Printable auto> ptr = std::make_unique<int>(5);
-    test(1);
-    test("test");
-    test('t');
 
-    test2(1);
-    test2("test");
-    test2('t');
+    Object object{42};
+    Object copy{object};
+    Object movedCopy{std::move(copy)};
+    Object copyAssigned = object;
+    Object moveAssigned = std::move(object);
 
-    test3(1);
-    test3("test");
-    test3('t');
-
-    struct MyType{};
-//    std::unordered_map<MyType, int> m;
-
-    MyConcept auto c = initMyConcept();
-
-    Int auto i = 5;
-//    Int auto b = "r";
-    std::cout << "Hello, World!" << std::endl;
+    //////////////////////////////////////////////////
 
 
     Object o1{5};
     Object o2{3.141483647};
-    Object o3{"o2"};
-    Object o4{std::string("o2")};
+//    Object o3{"o2"};
+//    Object o4{std::string("o2")};
 //    Object o4{MyType{}};
     dump(o1, std::cout);
     dump(o2, std::cout);
-    dump(o3, std::cout);
-    dump(o4, std::cout);
+//    dump(o3, std::cout);
+//    dump(o4, std::cout);
 
     std::vector<Object> vec{};
 //    vec.emplace_back(3.141483647);
--- src/state.h
+++ src/state.h
@@ -2,55 +2,50 @@
 
 #include <concepts>
 #include <memory>
-//#include <any>
-//#include <functional>
 #include <fmt/core.h>
 
 #include <iostream>
 
-template<typename T>
-concept State = std::regular<T>;
-//concept State = true;
+
+namespace Concept
+{
+
 
 template<typename T>
 concept Number = std::integral<T> or std::floating_point<T>;
 
-template<typename T>
-concept String = requires (T str) {
-    std::string_view{str};
+void dump(Number auto val, std::ostream& out) {
+    out << fmt::format("Number = {}\n", val);
+}
+
+template<typename T> concept State =
+        std::copyable<T>
+        and requires (T value, std::ostream& out) {
+            dump(value, out);
 };
-static_assert(String<std::string>);
-static_assert(String<std::string_view>);
-static_assert(String<char*>);
-static_assert(not String<char>);
 
-//void dump(Number auto val, std::ostream& out) {
-//    out << fmt::format("Number = {}\n", val);
-//}
-
-void dump(String auto val, std::ostream& out) {
-    out << fmt::format("String = \"{}\"\n", val);
-}
-
-void dump(auto const& val, std::ostream& out) {
-    out << fmt::format("{} = {}\n", typeid(val).name(), val);
-}
-
-class Object {
-    public:
+class Object
+{
+public:
 
     ~Object() = default;
-
-    template<State T>
-    explicit Object(T val)
-    : value_{std::make_unique<Model<T>>(std::move(val))}
-    {}
-
-    friend void dump(Object const& val, std::ostream& out) {
-        val.value_->dump_(out);
+    Object(Object&&) noexcept = default;
+    Object& operator=(Object&&) noexcept = default;
+    Object(Object const& obj) : state_{obj.state_->copy() } {}
+    Object& operator=(Object const& obj) {
+        auto tmp{ obj };
+        state_ = std::move(tmp.state_);
+        return *this;
     }
 
-    private:
+    template<State T> explicit Object(T state)
+        : state_{std::make_unique<Model < T>>(std::move(state)) } {}
+
+    friend void dump(Object const& val, std::ostream& out) {
+        val.state_->dump_(out);
+    }
+
+private:
 
     struct ConceptInterface;
     using Concept = std::unique_ptr<ConceptInterface>;
@@ -61,21 +56,27 @@ class Object {
         virtual void dump_(std::ostream&) = 0;
     };
 
-    template <State T>
-    struct Model final : ConceptInterface {
-        explicit Model(T value) : data{std::move(value)} {}
+    template<State T> struct Model final: ConceptInterface {
+        explicit Model(T state) : data_{std::move(state) } {}
         ~Model() final = default;
+
         [[nodiscard]] Concept copy() const final {
             return std::make_unique<Model>(*this);
         }
+
         void dump_(std::ostream& stream) final {
-            dump(data, stream);
+            dump(data_, stream);
         }
 
-        T data;
+        T data_;
     };
 
-    std::unique_ptr<ConceptInterface> value_;
+    Concept state_;
 };
 
-//static_assert(State<Object>);
+static_assert(std::movable<Object>);
+static_assert(std::copyable<Object>);
+static_assert(State<Object>);
+
+
+} //namespace Concept;
