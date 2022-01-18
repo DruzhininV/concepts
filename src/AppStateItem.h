7f84015db1826002279c96935e826c06af05182a
Vladimir Druzhinin <lvladimirdruzhininl@gmail.com>
2022-01-18 10:27:47 +0300

    the history and storage_policy

--- src/AppStateItem.h
+++ src/AppStateItem.h
@@ -17,7 +17,7 @@
 namespace app {
 
 template <typename T>
-concept Streamable = requires(T value, std::ostream out) {
+concept Streamable = requires(T const& value, std::ostream out) {
     { out << value } -> std::convertible_to<std::ostream&>;
 };
 
@@ -26,15 +26,44 @@ void dump(Streamable auto const& value) {
 }
 
 template<typename T>
-concept AppState =
-    std::copyable<T>
+concept app_state = std::copyable<T>
     and requires (T value) {
         dump(value);
 };
 
+struct UniqueStoragePolicy {
+    template<typename Concept>
+    using storage_type = std::unique_ptr<Concept>;
 
-class AppStateItem {
+    template<typename Model>
+    static auto copy_model(Model const& model) {
+        return std::make_unique<Model>(model);
+    }
+
+    template<typename Model, app_state Value, typename... Args>
+    requires std::constructible_from<Model, Value, Args...>
+    static auto create_model(Value value, Args... args) {
+        return std::make_unique<Model>(std::move(value), std::forward<Args...>(args)...);
+    }
+};
+
+template<typename T>
+concept storage_policy = requires(int value) {
+    typename T::template storage_type<int>;
+    { T::template create_model<int>(value) } -> std::same_as<typename T::template storage_type<int>>;
+    { T::template copy_model<int>(value) } -> std::same_as<typename T::template storage_type<int>>;
+};
+
+static_assert(storage_policy<UniqueStoragePolicy>);
+
+
+template<storage_policy StoragePolicy = UniqueStoragePolicy>
+class AppStateItem : private StoragePolicy {
 public:
+    friend void dump(AppStateItem const& val) {
+        val.model_->dump_();
+    }
+
     ~AppStateItem() = default;
     AppStateItem(AppStateItem&&) noexcept = default;
     AppStateItem& operator=(AppStateItem&&) noexcept = default;
@@ -45,46 +74,44 @@ public:
         return *this;
     }
 
-    template<AppState T> explicit AppStateItem(T state)
-        : model_{ std::make_unique<AppStateModel<T>>(std::move(state)) } {}
+    template<app_state T>
+    requires (not std::same_as<T, AppStateItem>)
+    explicit AppStateItem(T state)
+        : model_{ create_model<AppStateModel<T>>(std::move(state)) } {}
 
     template<typename T, typename DumpStrategy>
-    requires std::invocable<DumpStrategy, T>
+    requires std::invocable<DumpStrategy, T> and (not std::same_as<T, AppStateItem>)
     AppStateItem(T state, DumpStrategy strategy)
-        : model_{ std::make_unique<CustomizableModel<T, DumpStrategy>>(std::move(state), std::move(strategy))} {}
-
-    friend void dump(AppStateItem const& val /*, Args*/) {
-        val.model_->dump_(/*Args*/);
-    }
+        : model_{ create_model<CustomizableModel<T, DumpStrategy>>(std::move(state), std::move(strategy))} {}
 
 private:
 
     struct AppStateConcept;
-    using Concept = std::unique_ptr<AppStateConcept>;
+    using Concept = typename StoragePolicy::template storage_type<AppStateConcept>;
 
     struct AppStateConcept {
         virtual ~AppStateConcept() = default;
-        virtual Concept copy() const = 0;
-        virtual void dump_(/*Args*/) = 0;
+        virtual Concept copy()  const = 0;
+        virtual void dump_() const = 0;
     };
 
-    template<AppState T>
-    struct AppStateModel final: AppStateConcept {
+    template<typename T>
+struct AppStateModel final: AppStateConcept {
         explicit AppStateModel(T state) : data_{ std::move(state) } {}
         ~AppStateModel() final = default;
 
         [[nodiscard]] Concept copy() const final {
-            return std::make_unique<AppStateModel>(*this);
+            return copy_model<AppStateModel>(*this);
         }
 
-        void dump_(/*Args*/) final {
-            dump(data_ /*, Args*/);
+        void dump_() const final {
+            dump(data_);
         }
 
         T data_;
     };
 
-    template<AppState T, typename DumpStrategy>
+    template<app_state T, typename DumpStrategy>
     requires std::invocable<DumpStrategy, T>
     struct CustomizableModel final : AppStateConcept {
         CustomizableModel(T state, DumpStrategy strategy)
@@ -92,10 +119,10 @@ private:
         ~CustomizableModel() final = default;
 
         [[nodiscard]] Concept copy() const final {
-            return std::make_unique<CustomizableModel>(*this);
+            return copy_model<CustomizableModel>(*this);
         }
 
-        void dump_() final {
+        void dump_() const final {
             dumpStrategy_(data_);
         }
 
@@ -106,9 +133,9 @@ private:
     Concept model_;
 };
 
-static_assert(std::movable<AppStateItem>);
-static_assert(std::copyable<AppStateItem>);
-static_assert(AppState<AppStateItem>);
+static_assert(std::movable<AppStateItem<>>);
+static_assert(std::copyable<AppStateItem<>>);
+static_assert(app_state<AppStateItem<>>);
 
 template<typename T>
 concept std_like_container = std::copyable<T>
@@ -148,20 +175,20 @@ static_assert(std_like_container<std::array<int,1>>);
 static_assert(std_like_container<boost::circular_buffer<int>>);
 
 template<typename T>
-concept AppStateContainer = std_like_container<T>
-                            and std::same_as<AppStateItem, typename T::value_type>
-                            and AppState<typename T::value_type>;
+concept app_state_container = std_like_container<T>
+                              and std::same_as<AppStateItem<>, typename T::value_type>
+                              and app_state<typename T::value_type>;
 
-void dump(AppStateContainer auto container) {
+void dump(app_state_container auto const& container) {
     std::cout << "Container begin\n";
-    std::ranges::for_each(container, [&](AppState auto const& state) {
+    std::ranges::for_each(container, [&](app_state auto const& state) {
         dump(state);
     });
     std::cout << "Container end\n";
 }
 
 
-static_assert(AppStateContainer<std::vector<AppStateItem>>);
-static_assert(not AppStateContainer<std::string>);
+//static_assert(app_state_container<std::vector<AppStateItem>>);
+//static_assert(not app_state_container<std::string>);
 
 } //namespace app;
--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,93 @@
 #include "AppStateItem.h"
 using namespace app;
 #include <vector>
+#include <numbers>
+#include <thread>
+#include <future>
 
 
-struct MyType{};
+struct MyType{
+    MyType() = default;
+    ~MyType() = default;
+    MyType(MyType const&) {
+        std::cout << "MyType copied\n";
+    }
+    MyType& operator=(MyType const&) {
+        std::cout << "MyType assigned\n";
+        return *this;
+    }
+    MyType(MyType&&)  noexcept {
+        std::cout << "MyType moved\n";
+    }
+    MyType& operator=(MyType&&)  noexcept {
+        std::cout << "MyType move assigned\n";
+        return *this;
+    }
+};
 
-void dump(MyType value) {
+void dump(MyType const& value) {
     std::cout << "MyType\n";
 }
 
+//using State = std::vector<AppStateItem<UniqueStoragePolicy>>;
+using State = std::vector<AppStateItem<>>;
+using History = boost::circular_buffer<State>;
+
+History createHistory(size_t capacity) {
+    History history{capacity};
+    history.push_back({});
+    return history;
+}
+
+void commit(History& history) {
+    assert(!history.empty());
+    history.push_back(history.back());
+}
+
+void undo(History& history) {
+    assert(!history.empty());
+    history.pop_back();
+}
+
+State& current(History& history) {
+    assert(!history.empty());
+    return history.back();
+}
 
 int main()
 {
-    //////////////////////////////////////////////////
-
-    AppStateItem object{42};
-    AppStateItem copy{object};
-    AppStateItem movedCopy{std::move(copy)};
-    AppStateItem copyAssigned = object;
-    AppStateItem moveAssigned = std::move(object);
-
-    //////////////////////////////////////////////////
-
-
-
-    std::vector<AppStateItem> appState{};
-    using AppStateHistory = boost::circular_buffer<AppStateItem>;
-
-    auto dbDumper{[](AppState auto const& state){
+    auto dbDumper{[](app_state auto const& state){
         std::cout << fmt::format("Dump into DataBase {}\n", state);
     }};
 
-    appState.emplace_back(0, dbDumper);
-    appState.emplace_back(1.1, dbDumper);
-    appState.emplace_back("2", dbDumper);
-    appState.emplace_back(appState);
-    appState.emplace_back(std::string{"string"}, dbDumper);
-    appState.emplace_back(MyType{});
-    appState.emplace_back(42, [](AppState auto const& state){
-        std::cout << fmt::format("Dump into DataBase: {}\n", state);
-    });
+    auto history{createHistory(10)};
 
-    AppStateHistory history{10};
+    current(history).emplace_back(1);
+    current(history).emplace_back(std::numbers::pi);
+    current(history).emplace_back("string");
 
+    std::cout << fmt::format("{:=^50}\n", "State 1");
+    dump(current(history));
 
-    dump(appState);
+    commit(history);
+
+    current(history)[0] = AppStateItem(42);
+    current(history)[2] = AppStateItem("new string", dbDumper);
+    current(history).emplace_back(current(history));
+    current(history).emplace_back(MyType{});
+
+    auto asyncDump{std::async([state = current(history)]{
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << fmt::format("{:=^50}\n", "Async dump");
+        dump(state);
+    })};
+
+    std::cout << fmt::format("{:=^50}\n", "State 2");
+    dump(current(history));
+
+    std::cout << fmt::format("{:=^50}\n", "Undo");
+    undo(history);
+    dump(current(history));
 
 
     return 0;
